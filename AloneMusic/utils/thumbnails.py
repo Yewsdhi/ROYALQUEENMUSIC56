c6e67739608a5e7bf09f2cff6f5f78434d750128
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-02-18 15:21:02 +0530

    Add files via upload

--- AloneMusic/utils/thumbnails.py
+++ AloneMusic/utils/thumbnails.py
@@ -1,204 +1,174 @@
-#
-# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
-#
-# This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
-#
-# All rights reserved.
+# a part of Opus Music Project 2026 ©
+# this code is & will be our property as it is or even after modified 
+# must give credits to @x_ifeelram  if used this code anywhere ever 
 
 import os
 import re
-
-import aiofiles
+import random
 import aiohttp
-from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
-from py_yt import VideosSearch
+import aiofiles
+import traceback
 
+from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont, ImageOps
+from youtubesearchpython.future import VideosSearch
 from config import YOUTUBE_IMG_URL
 
 
+try:
+    RESAMPLE = Image.Resampling.LANCZOS
+except AttributeError:
+    try:
+        RESAMPLE = Image.LANCZOS
+    except AttributeError:
+        RESAMPLE = Image.ANTIALIAS
+
+
 def changeImageSize(maxWidth, maxHeight, image):
-    widthRatio = maxWidth / image.size[0]
-    heightRatio = maxHeight / image.size[1]
-    newWidth = int(widthRatio * image.size[0])
-    newHeight = int(heightRatio * image.size[1])
-    newImage = image.resize((newWidth, newHeight))
-    return newImage
+    ratio = min(maxWidth / image.size[0], maxHeight / image.size[1])
+    newSize = (int(image.size[0] * ratio), int(image.size[1] * ratio))
+    return image.resize(newSize, RESAMPLE)
 
 
-async def get_thumb(videoid):
-    if os.path.isfile(f"cache/{videoid}.png"):
-        return f"cache/{videoid}.png"
+def truncate(text, max_chars=50):
+    words = text.split()
+    text1, text2 = "", ""
+    for word in words:
+        if len(text1 + " " + word) <= max_chars and not text2:
+            text1 += " " + word
+        else:
+            text2 += " " + word
+    return [text1.strip(), text2.strip()]
 
+
+def add_rounded_corners(im, radius):
+    circle = Image.new('L', (radius * 2, radius * 2), 0)
+    draw = ImageDraw.Draw(circle)
+    draw.ellipse((0, 0, radius * 2, radius * 2), fill=255)
+    alpha = Image.new('L', im.size, 255)
+    w, h = im.size
+    alpha.paste(circle.crop((0, 0, radius, radius)), (0, 0))
+    alpha.paste(circle.crop((0, radius, radius, radius * 2)), (0, h - radius))
+    alpha.paste(circle.crop((radius, 0, radius * 2, radius)), (w - radius, 0))
+    alpha.paste(circle.crop((radius, radius, radius * 2, radius * 2)), (w - radius, h - radius))
+    im.putalpha(alpha)
+    return im
+
+
+def fit_text(draw, text, max_width, font_path, start_size, min_size):
+    size = start_size
+    while size >= min_size:
+        font = ImageFont.truetype(font_path, size)
+        if draw.textlength(text, font=font) <= max_width:
+            return font
+        size -= 1
+    return ImageFont.truetype(font_path, min_size)
+
+
+def create_rounded_square(image, size, radius=50):
+    image = image.resize((size, size), RESAMPLE).convert("RGBA")
+    rounded_mask = Image.new("L", (size, size), 0)
+    draw = ImageDraw.Draw(rounded_mask)
+    draw.rounded_rectangle((0, 0, size, size), radius=radius, fill=255)
+    rounded_image = Image.new("RGBA", (size, size))
+    rounded_image.paste(image, (0, 0), mask=rounded_mask)
+    return rounded_image
+
+
+async def _get_fallback_thumb_local():
+    fallback_path = "cache/fallback_thumb.png"
+    try:
+        if not os.path.exists("cache"):
+            os.makedirs("cache", exist_ok=True)
+        if not os.path.exists(fallback_path):
+            async with aiohttp.ClientSession() as session:
+                async with session.get(YOUTUBE_IMG_URL) as resp:
+                    if resp.status == 200:
+                        f = await aiofiles.open(fallback_path, mode="wb")
+                        await f.write(await resp.read())
+                        await f.close()
+        if os.path.exists(fallback_path):
+            return fallback_path
+    except:
+        traceback.print_exc()
+    return None
+
+
+async def get_thumb(videoid: str):
     url = f"https://www.youtube.com/watch?v={videoid}"
     try:
         results = VideosSearch(url, limit=1)
         for result in (await results.next())["result"]:
-            try:
-                title = result["title"]
-                title = re.sub(r"\W+", " ", title)
-                title = title.title()
-            except:
-                title = "Unsupported Title"
-            try:
-                duration = result["duration"]
-            except:
-                duration = "Unknown"
+            title = re.sub(r"\W+", " ", result.get("title", "UTitle")).title()
+            duration = result.get("duration", "Unknown Min")
             thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-            try:
-                views = result["viewCount"]["short"]
-            except:
-                views = "Unknown Views"
+            views = result.get("viewCount", {}).get("short", "Nil Views")
+            channel = result.get("channel", {}).get("name", "Unknown Channel")
 
         async with aiohttp.ClientSession() as session:
             async with session.get(thumbnail) as resp:
                 if resp.status == 200:
+                    if not os.path.exists("cache"):
+                        os.makedirs("cache", exist_ok=True)
                     f = await aiofiles.open(f"cache/thumb{videoid}.png", mode="wb")
                     await f.write(await resp.read())
                     await f.close()
 
         youtube = Image.open(f"cache/thumb{videoid}.png")
-
-        GLOW_COLOR = "#ff0099"  # Neon Pink
-        BORDER_COLOR = "#FF1493"  # Deep Pink
         image1 = changeImageSize(1280, 720, youtube)
-        image1 = image1.filter(ImageFilter.GaussianBlur(20))
-        image1 = ImageEnhance.Brightness(image1).enhance(0.4)
+        image2 = image1.convert("RGBA")
 
-        thumb_width = 840
-        thumb_height = 460
+        gradient = Image.new("RGBA", image2.size, (0, 0, 0, 255))
+        enhancer = ImageEnhance.Brightness(image2.filter(ImageFilter.GaussianBlur(6)))
+        blurred = enhancer.enhance(0.6)
+        background = Image.alpha_composite(gradient, blurred)
 
-        youtube_thumb = youtube.resize((thumb_width, thumb_height))
+        logo = create_rounded_square(youtube, 450, radius=60)
+        background.paste(logo, (100, 150), logo)
 
-        mask = Image.new("L", (thumb_width, thumb_height), 0)
-        draw_mask = ImageDraw.Draw(mask)
-        draw_mask.rounded_rectangle(
-            [(0, 0), (thumb_width, thumb_height)], radius=20, fill=255
-        )
-        youtube_thumb.putalpha(mask)
-        center_x = 640
-        center_y_img = 300
-        thumb_x = center_x - (thumb_width // 2)
-        thumb_y = center_y_img - (thumb_height // 2)
-        thumb_x2 = thumb_x + thumb_width
-        thumb_y2 = thumb_y + thumb_height
+        draw = ImageDraw.Draw(background)
+        font_info = ImageFont.truetype("BillaMusic/assets/font2.ttf", 28)
+        font_time = ImageFont.truetype("BillaMusic/assets/font2.ttf", 26)
+        font_path = "BillaMusic/assets/font3.ttf"
 
-        glow_layer = Image.new("RGBA", (1280, 720), (0, 0, 0, 0))
-        draw_glow = ImageDraw.Draw(glow_layer)
+        title_max_width = 540
+        title_lines = truncate(title, 35)
 
-        glow_expand = 20
-        draw_glow.rounded_rectangle(
-            [
-                (thumb_x - glow_expand, thumb_y - glow_expand),
-                (thumb_x2 + glow_expand, thumb_y2 + glow_expand),
-            ],
-            radius=30,
-            fill=GLOW_COLOR,
-        )
-        glow_layer = glow_layer.filter(ImageFilter.GaussianBlur(30))
-        image1.paste(glow_layer, (0, 0), glow_layer)
-        border_layer = Image.new("RGBA", (1280, 720), (0, 0, 0, 0))
-        draw_border = ImageDraw.Draw(border_layer)
+        title_font1 = fit_text(draw, title_lines[0], title_max_width, font_path, 42, 28)
+        draw.text((565, 180), title_lines[0], (255, 255, 255), font=title_font1)
 
-        border_expand = 5
-        draw_border.rounded_rectangle(
-            [
-                (thumb_x - border_expand, thumb_y - border_expand),
-                (thumb_x2 + border_expand, thumb_y2 + border_expand),
-            ],
-            radius=25,
-            fill=BORDER_COLOR,
-        )
-        image1.paste(border_layer, (0, 0), border_layer)
+        if title_lines[1]:
+            title_font2 = fit_text(draw, title_lines[1], title_max_width, font_path, 36, 24)
+            draw.text((565, 225), title_lines[1], (220, 220, 220), font=title_font2)
 
-        image1.paste(youtube_thumb, (thumb_x, thumb_y), youtube_thumb)
+        draw.text((565, 305), f"{channel} | {views}", (240, 240, 240), font=font_info)
 
-        draw = ImageDraw.Draw(image1)
+        rand = (random.randint(100, 255), random.randint(100, 255), random.randint(100, 255))
+        draw.line([(565, 370), (990, 370)], fill=rand, width=6)
+        draw.ellipse([(990, 362), (1010, 382)], outline=rand, fill=rand, width=12)
+        draw.text((1080, 385), duration, (255, 255, 255), font=font_time)
 
-        try:
-            font_title = ImageFont.truetype("AloneMusic/assets/font.ttf", 45)
-            font_details = ImageFont.truetype("AloneMusic/assets/font2.ttf", 30)
-            font_watermark = ImageFont.truetype("AloneMusic/assets/font2.ttf", 25)
-        except:
-            font_title = ImageFont.truetype("arial.ttf", 45)
-            font_details = ImageFont.truetype("arial.ttf", 30)
-            font_watermark = ImageFont.truetype("arial.ttf", 25)
+        watermark_font = ImageFont.truetype("BillaMusic/assets/font2.ttf", 24)
+        watermark_text = "Billa=Space"
+        text_size = draw.textsize(watermark_text, font=watermark_font)
+        x = background.width - text_size[0] - 25
+        y = background.height - text_size[1] - 25
+        glow_pos = [(x + dx, y + dy) for dx in (-1, 1) for dy in (-1, 1)]
+        for pos in glow_pos:
+            draw.text(pos, watermark_text, font=watermark_font, fill=(0, 0, 0, 180))
+        draw.text((x, y), watermark_text, font=watermark_font, fill=(255, 255, 255, 240))
 
-        def get_text_width(text, font):
-            if hasattr(draw, "textlength"):
-                return draw.textlength(text, font=font)
-            else:
-                return draw.textsize(text, font=font)[0]
-
-        if len(title) > 45:
-            title = title[:45] + "..."
-
-        w_title = get_text_width(title, font_title)
-        text_y_pos = thumb_y2 + 50
-
-        draw.text(
-            ((1280 - w_title) / 2, text_y_pos),
-            text=title,
-            fill="white",
-            font=font_title,
-            stroke_width=1,
-            stroke_fill="black",
-        )
-
-        stats_text = f"YouTube : {views} | Time : {duration} | Player : @aashikmusicbot"
-        w_stats = get_text_width(stats_text, font_details)
-        draw.text(
-            ((1280 - w_stats) / 2, text_y_pos + 70),
-            text=stats_text,
-            fill=BORDER_COLOR,
-            font=font_details,
-            stroke_width=1,
-            stroke_fill="black",
-        )
-
-        text_classy = "AloneMusic"
-        w_classy = get_text_width(text_classy, font_watermark)
-
-        draw.text(
-            (1280 - w_classy - 30, 30),
-            text=text_classy,
-            fill="yellow",
-            font=font_watermark,
-            stroke_width=1,
-            stroke_fill="black",
-        )
-
-        draw.text(
-            (30, 680),
-            text="AloneMusic",
-            fill="white",
-            font=font_watermark,
-            stroke_width=1,
-            stroke_fill="black",
-        )
+        background = add_rounded_corners(background, 30)
 
         try:
             os.remove(f"cache/thumb{videoid}.png")
         except:
             pass
 
-        file_name = f"cache/{videoid}.png"
-        image1.save(file_name)
-        return file_name
+        tpath = f"cache/{videoid}.png"
+        background.save(tpath)
+        return tpath
 
-    except Exception as e:
-        print(e)
-        return YOUTUBE_IMG_URL
-
-
-async def get_qthumb(vidid):
-    try:
-        url = f"https://www.youtube.com/watch?v={vidid}"
-        results = VideosSearch(url, limit=1)
-        for result in (await results.next())["result"]:
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-        return thumbnail
-    except Exception as e:
-        print(e)
-        return YOUTUBE_IMG_URL
+    except:
+        traceback.print_exc()
+        fallback = await _get_fallback_thumb_local()
+        return fallback
